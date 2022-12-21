d402861e8afe3c18f639ab628ed9d7ec49043cd8
ViktorMathe <matheviktor@icloud.com>
2022-12-21 23:38:00 +0000

    Added checkout success html

--- checkout/signals.py
+++ checkout/signals.py
@@ -6,11 +6,11 @@ from .models import OrderLineItem
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    
+
     instance.order.update_total()
 
 
 @receiver(post_delete, sender=OrderLineItem)
 def delete_on_save(sender, instance, created, **kwargs):
-    
+
     instance.order.update_total()
