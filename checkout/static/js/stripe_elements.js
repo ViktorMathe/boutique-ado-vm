cc6888e72a7d753427d1c922b91a24ee7ccdf74a
ViktorMathe <matheviktor@icloud.com>
2022-12-21 14:54:55 +0000

    Added basic checkout function

--- checkout/static/js/stripe_elements.js
+++ checkout/static/js/stripe_elements.js
@@ -1,27 +1,75 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 const style = {
     theme: 'stripe',
     base: {
-      colorPrimary: '#000000',
-      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-      fontSmoothing: 'antialiased',
-      fontSize: '16px',
-      '::placeholder': {
-        color: '#aab7c4'
-      }
+        colorPrimary: '#000000',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
     rules: {
-    '.Input--invalid': {
-        boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
-        color: '#dc3545',
-        iconColor: '#dc3545'
-      },
+        '.Input--invalid': {
+            boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.07), 0 0 0 2px var(--colorDanger)',
+            color: '#dc3545',
+            iconColor: '#dc3545'
+        },
 
-      // See all supported class names and selector syntax below
+        // See all supported class names and selector syntax below
     }
 };
-var card = elements.create('card',  {style: style});
-card.mount('#card-element',);
+var card = elements.create('card', {
+    style: style
+});
+card.mount('#card-element', );
+
+//Handle realtime validation errors on the card element
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class='icon' role='alert'>
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+            `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+//Handle form submit 
+const form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
