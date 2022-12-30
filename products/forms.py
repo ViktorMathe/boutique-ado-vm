899d7af677246fbf8809ff4c00028f01b456bd55
ViktorMathe <matheviktor@icloud.com>
2022-12-30 20:28:50 +0000

    finished add product function

--- products/forms.py
+++ products/forms.py
@@ -3,15 +3,16 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+
     class Meta:
         model = Product
         fields = '__all__'
 
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            categories = Category.objects.all()
-            freindly_names = [(c.id, c.get_friendly_name()) for c in categories]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-            self.fields['category'].choices = freindly_names
-            for field_name, field in self.fields.items():
-                field.widget.attrs['class'] = 'border-black rounded-0'
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
