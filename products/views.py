7c549915cfa6a3b197409907dc13442af839c733
ViktorMathe <matheviktor@icloud.com>
2023-01-01 17:57:35 +0000

    Fixing the image field

--- products/views.py
+++ products/views.py
@@ -105,7 +105,7 @@ def add_product(request):
 def edit_product(request, product_id):
     """ Edit a product in the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only stora owners can do that')
+        messages.error(request, 'Sorry, only store owners can do that')
         return redirect(reverse('home'))
 
     product = get_object_or_404(Product, pk=product_id)
